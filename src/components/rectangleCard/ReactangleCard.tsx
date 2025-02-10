392f2350c3f875a11e58b5504f5cf6cffdd6f4a9
ricardoareiaslinhares <ricardolinhares.dev@gmail.com>
2025-02-10 23:57:17 +0000

    feat:rectangle card for homepage

--- src/components/rectangleCard/ReactangleCard.tsx
+++ src/components/rectangleCard/ReactangleCard.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import Image from 'next/image'
-type Props = {}
+import React from "react";
+import Image from "next/image";
+type Props = {};
 
 const ReactangleCard = (props: Props) => (
-    <div className='w-full min-h-[400px] flex flex-1 flex-col'>
-        
+    <div className="">
+
+  <div className="w-full min-h-[400px] flex flex-1 flex-row justify-end items-center">
+    <div className="z-10 flex w-[600px] h-[200px] bg-slate-100 transform translate-x-12">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum veniam
+      quas modi, dolorem eos corrupti nulla provident iste quam accusantium ab
+      a, ea qui sapiente cum eum praesentium, iusto culpa.
+    </div>
     <Image
       width={0}
       height={0}
@@ -13,10 +19,10 @@ const ReactangleCard = (props: Props) => (
       sizes="100vw"
       src="/hero.jpg"
       alt="hero"
-      className="bg-cover bg-center bg-no-repeat absolute -z-1 object-cover w-[500px] h-[300px] brightness-90 transition-transform duration-200"
-    />
-    
-    </div>
-)
+      className="bg-cover bg-center bg-no-repeat -z-1 object-cover w-[500px] h-[300px] brightness-90 transition-transform duration-200"
+      />
+      </div>
+  </div>
+);
 
-export default ReactangleCard
+export default ReactangleCard;
