b20a5289263dabd7043a71b16aebf3bb2de2f5f6
ricardoareiaslinhares <ricardolinhares.dev@gmail.com>
2025-02-11 01:48:00 +0000

    reactangleCard - added animation

--- src/components/rectangleCard/ReactangleCard.tsx
+++ src/components/rectangleCard/ReactangleCard.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 type Props = {};
 import { motion, inView, useInView } from "framer-motion";
@@ -10,10 +10,11 @@ const ReactangleCard = (props: Props) => {
   const isInViewImage = useInView(refImage, { once: true });
   const refText = useRef(null);
   const isInViewText = useInView(refText, { once: true });
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div className="">
-      <div className="w-full min-h-[400px] flex flex-1 flex-row justify-end items-center mt-20">
+      <div className="w-full min-h-[400px] flex flex-1 flex-row justify-end items-center mt-12">
         <motion.div
           ref={refText}
           initial={{ x: -150, opacity: 0 }}
@@ -21,11 +22,38 @@ const ReactangleCard = (props: Props) => {
           transition={{ duration: 0.5 }}
           className="z-10 "
         >
-          <div className="flex w-[600px] h-[200px] bg-slate-100 transform translate-x-12">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum
-            veniam quas modi, dolorem eos corrupti nulla provident iste quam
-            accusantium ab a, ea qui sapiente cum eum praesentium, iusto culpa.
-          </div>
+          <motion.div
+            whileHover={{ height: 340 }}
+            transition={{ duration: 0.3 }}
+            className=" overflow-hidden translate-x-12 flex items-center bg-slate-200 flex-1 flex-col justify-center "
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            <p className="w-[600px] h-[100px] text-center p-4 ">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum
+              veniam quas modi, dolorem eos corrupti nulla provident iste quam
+              accusantium ab a, ea qui sapiente cum eum praesentium, iusto
+              culpa.
+            </p>
+            <motion.div
+              initial={{ opacity: 0, y: -10, position: "absolute", color: "transparent" }} // Hidden by default
+              animate={{
+                opacity: isHovered ? 1 : 0,
+                y: isHovered ? 0 : -10,
+                color: isHovered ? "black" : "transparent",
+                position: isHovered ? "relative" : "absolute",
+              }}
+                exit={{ opacity: 0, y: -10 }} 
+              transition={{ duration: 0.3 }}
+              className="opacity-0 text-center mt-4"
+            >
+              <p className=" w-[600px] text-center p-4">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum
+
+              </p>
+
+            </motion.div>
+          </motion.div>
         </motion.div>
         <motion.div
           ref={refImage}
@@ -34,6 +62,7 @@ const ReactangleCard = (props: Props) => {
           transition={{ duration: 0.5 }}
           className="-z-1"
         >
+            
           <Image
             width={0}
             height={0}
